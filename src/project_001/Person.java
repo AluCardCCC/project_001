df7432ddf4dcecf9062807b80388fd34366e1b1a
alucard.ci <alucard.ci@bytedance.com>
2019-07-21 22:47:46 +0800

    add

---
+++ src/project_001/Person.java
@@ -0,0 +1,21 @@
+package project_001;
+
+public class Person {
+    protected String name;
+    protected String sex;
+    protected int age;
+    protected String getName(){
+        return name;
+    }
+    protected void setName(String name){
+        this.name=name;
+    }
+    public String toString(){
+        return "this name is "+ name;
+    }
+    Person(String name){
+        System.out.println("this person name is "+name);
+
+    }
+
+}
