f055aee605a68270fdaedfdbe1b806dd7fa6e995
alucard.ci <alucard.ci@bytedance.com>
2019-07-27 14:35:02 +0800

    add collection test

--- src/project_001/ArrayTest2.java
+++ src/project_001/ArrayTest2.java
@@ -1,4 +1,18 @@
 package project_001;
 
+import java.util.*;
+
 public class ArrayTest2 {
+    public static void main(String args[]){
+        List list1 = new ArrayList();
+        list1.add(1);
+        int a [] = new int [] {1,2,3};
+        int b [] = new int [] {1,2,3};
+        System.out.println(a);
+        list1.add(a);
+        System.out.println(list1);
+        System.out.println(list1.size());
+        System.out.println(list1.get(1));
+        System.out.println(list1.contains(b));
+    }
 }
--- src/project_001/CollectionTest.java
+++ src/project_001/CollectionTest.java
@@ -1,4 +1,22 @@
 package project_001;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class CollectionTest {
+    public static void main(String args[]){
+        List list1 = new LinkedList();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        System.out.println(list1);
+        int  a;
+        a=Integer.parseInt(list1.get(1).toString());
+        System.out.println(a);
+        System.out.println(list1.get(1).getClass());
+        for (Object elem: list1){
+            System.out.println(elem);
+        }
+
+    }
 }
