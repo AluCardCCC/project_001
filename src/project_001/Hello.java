f6070023285d2d518808a3afeea11ae89c4389db
alucard.ci <alucard.ci@bytedance.com>
2020-02-04 18:03:25 +0800

    change hello

--- src/project_001/Hello.java
+++ src/project_001/Hello.java
@@ -1,5 +1,7 @@
 package project_001;
 
+import java.lang.reflect.Type;
+
 public class Hello {
 
     private Hello(){
@@ -11,6 +13,9 @@ public class Hello {
     public String pp(){
         System.out.println("123123141");
         return "12314141p";
+    }
+    private static String GetType(Object o){
+        return o.getClass().toString();
     }
 
     public static void main (String args[]){
@@ -25,7 +30,11 @@ public class Hello {
         y=(x==10)?1:0;
         System.out.println("value x:"+ x);
         System.out.println("value y:" + y);
-
-
+        System.out.println("char test\n ");
+        Character ch = new Character('a');
+        char ch2= 'b';
+        System.out.println(ch);
+        System.out.println(ch.getClass().getName());
+        System.out.println(GetType(ch2));
     }
 }
