34d7fc4bdc5c52e48164b71b0d328ed0d5d4b6c8
alucard.ci <alucard.ci@bytedance.com>
2020-03-08 14:48:27 +0800

    add datedemo

---
+++ src/project_001/DateDemo.java
@@ -0,0 +1,9 @@
+package project_001;
+import java.util.Date;
+
+public class DateDemo {
+    public static void main(String args []){
+        Date date1 = new Date();
+        System.out.println(date1.toString());
+    }
+}
